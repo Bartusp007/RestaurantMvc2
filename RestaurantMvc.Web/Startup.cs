9b381bad4f928d3114d7e6c41ed2adb5800ed92e
Bartusp007 <41994395+Bartusp007@users.noreply.github.com>
2019-01-09 14:26:34 +0100

    Add files via upload

---
+++ RestaurantMvc.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantMvc.Web.Startup))]
+namespace RestaurantMvc.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
